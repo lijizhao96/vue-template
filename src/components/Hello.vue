29f81df7e88ef33d1917cb924b215c369800fb56
lijizhao <lijizhao1996@gmail.com>
2023-01-05 13:51:36 +0800

    Initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="hello">Hello</div>
+</template>
+<style lang="scss" scoped>
+.hello {
+    width: 100vw;
+    line-height: 100vh;
+    text-align: center;
+    font-size: 40px;
+    font-weight: 600;
+}
+</style>
